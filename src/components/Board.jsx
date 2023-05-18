a44e96e60ea1786e6a6f13e198349619a3354812
Aakanshya-Gahatraj <aakanshya.gahatraj707@gmal.com>
2023-05-18 13:08:45 +0545

    tic tac toe completed

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,24 +3,55 @@ import Cell from "./Cell";
 const TEST_BOARD_STATE = ["X", "O", "", "", "X", "", "", "", "X"];
 const whoWon = (boardState = TEST_BOARD_STATE) => {
   //write logic
-  if (diagonalCheck(boardState, "X")) return "X";
-  if (diagonalCheck(boardState, "O")) return "O";
+  const diagonalSameVal = diagonalCheck(boardState);
+  const straightSameVal = straightCheck(boardState);
+
+  console.log({ diagonalSameVal, straightSameVal });
+
+  if (diagonalSameVal) {
+    return diagonalSameVal;
+  }
+  if (straightSameVal) {
+    return straightSameVal;
+  }
+
   // good luck
   return null;
 };
 
-const diagonalCheck = (boardState, value) => {
-  // write that returns true if won
-  if (boardState[4] == value) {
-    const diagonalRight =
-      boardState[0] == boardState[4] && boardState[4] == boardState[8];
-    const diagonalLeft =
-      boardState[2] == boardState[4] && boardState[4] == boardState[6];
-
-    console.log({ diagonalLeft, diagonalRight, value });
-    return diagonalLeft || diagonalRight;
+const straightCheck = (boardState) => {
+  console.table(boardState);
+  if (
+    (boardState[0] == boardState[3] && boardState[3] == boardState[6]) ||
+    (boardState[0] == boardState[1] && boardState[1] == boardState[2])
+  ) {
+    return boardState[0];
   }
-  return false;
+  if (
+    (boardState[1] == boardState[4] && boardState[4] == boardState[7]) ||
+    (boardState[3] == boardState[4] && boardState[4] == boardState[5])
+  ) {
+    return boardState[4];
+  }
+  if (
+    (boardState[2] == boardState[5] && boardState[5] == boardState[8]) ||
+    (boardState[6] == boardState[7] && boardState[7] == boardState[8])
+  ) {
+    return boardState[8];
+  }
+  return "";
+};
+
+const diagonalCheck = (boardState) => {
+  // write that returns true if won
+  const diagonalRight =
+    boardState[0] == boardState[4] && boardState[4] == boardState[8];
+  const diagonalLeft =
+    boardState[2] == boardState[4] && boardState[4] == boardState[6];
+
+  if (diagonalLeft || diagonalRight) return boardState[4];
+
+  return "";
 };
 
 const Board = () => {
@@ -28,7 +59,6 @@ const Board = () => {
   const [boardState, setBoardState] = useState(Array(9).fill(""));
 
   const winner = whoWon(boardState);
-  console.log({ boardState, winner });
   return (
     <>
       <h1 className="text-4xl">Turn : {turn}</h1>
@@ -36,8 +66,8 @@ const Board = () => {
         {boardState.map((cellValue, idx) => (
           <Cell
             key={idx}
-            value={cellValue + " " + idx}
-            disabled={cellValue !== ""}
+            value={cellValue}
+            disabled={cellValue !== "" || winner}
             onClickHandler={() => {
               const copyBoard = [...boardState];
               copyBoard[idx] = turn;
